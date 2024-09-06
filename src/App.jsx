2beb2cec37d20b7c765567820ff2ba4577ad97f3
ryan-taylor <ryan-taylor@users.noreply.github.com>
2024-09-05 20:19:23 -0400

    centered on page for demo

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const SwipeCards = () => {
 
   return (
     <div
-      className="relative h-screen w-full bg-neutral-100 flex items-center justify-center"
+      className="flex items-center justify-center min-h-screen w-full bg-neutral-100 dark:bg-neutral-800"
       style={{
         backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='2' stroke='%23d4d4d4'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
       }}
